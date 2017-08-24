9396b8f52194d9de7e3364b666c3551304960bc9
Andrew Johnson <andrew.johnson612@gmail.com>
2017-08-23 23:59:10 -0500

    Add header and footer partials. Improve landing page appearance

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
+
 app.use(express.static(__dirname + '/public'));
 
 // GET routes
 
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('landingPage');
+});
+
+app.get('/main', (req, res) => {
+	res.render('main');
 });
 
 app.listen(3000, () => {
